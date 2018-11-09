97e3b003ffb5b26ace66e190c0097bdd8866a63d
HeMixMvP <44098198+HeMixMvP@users.noreply.github.com>
2018-11-09 15:53:55 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -11,22 +11,4 @@ client.on('message', msg => {
   }
 });
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'm!help') {
-    msg.reply('بنضيف الأوامر لسة يا اخ');
-  }
-});
-
-
-
-
-
-
-client.login('Bot_Token');
+client.login('token');
